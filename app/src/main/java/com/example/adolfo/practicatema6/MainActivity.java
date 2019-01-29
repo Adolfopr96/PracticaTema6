913381a9942d93d861db8b7deaed739524345047
Adolfopr96 <adolfo.prado96@gmail.com>
2019-01-29 09:26:59 +0100

    Completadas las issues "Pantalla Principal" y "Pantalla Personas".

---
+++ app/src/main/java/com/example/adolfo/practicatema6/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.adolfo.practicatema6;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img1, img2;
+    Intent i1, i2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img1 = findViewById(R.id.img1_main);
+        img2 = findViewById(R.id.img2_main);
+        i1 = new Intent(this, MainPersonas.class);
+        //i2 = new Intent(this, MainBloc.class);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i1);
+            }
+        });
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Menu personas:
+            case R.id.menu_item1_main:
+                startActivity(i1);
+                break;
+            // Menu bloc de notas:
+            case R.id.menu_item2_main:
+                startActivity(i2);
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/adolfo/practicatema6/MainPersonas.java
@@ -0,0 +1,11 @@
+package com.example.adolfo.practicatema6;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+    public class MainPersonas extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.layout_main_personas);
+        }
+    }
