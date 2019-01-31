fcdf1fc211b197f1db1ac22b59a831463a2cdee2
Adolfopr96 <adolfo.prado96@gmail.com>
2019-01-31 10:55:32 +0100

    Se ha terminado la pantalla del loc de notas, se puede insertar texto multilinea y guardarlo en preferencias, cuando se vuelva a abrir la activity, se mostrará el texto almacenado anteriormente.

--- app/src/main/java/com/example/adolfo/practicatema6/MainBloc.java
+++ app/src/main/java/com/example/adolfo/practicatema6/MainBloc.java
@@ -1,13 +1,40 @@
 package com.example.adolfo.practicatema6;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainBloc extends AppCompatActivity {
+    EditText et_bloc;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_bloc);
+        sp = getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+        et_bloc = findViewById(R.id.et_bloc);
+        et_bloc.setText(sp.getString("et_bloc_texto", ""));
+
+    }
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("et_bloc_texto", et_bloc.getText().toString());
+        editor.commit();
+        return false;
+    }
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_guardar_editar, menu);
+        return true;
     }
 }
